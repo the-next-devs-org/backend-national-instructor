8b82de15fb203cbfdd610b2ab27546da7d525c78
The Next Devs <nextdevs6@gmail.com>
2025-08-17 15:43:35 +0500

    Update database.js

--- dbconfig/database.js
+++ dbconfig/database.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: process.env.DB_HOST || 'localhost',
     USER: process.env.DB_USER || 'root',
     PASSWORD: process.env.DB_PASS || '',   // use env DB_PASS
-    DB: process.env.DB || 'nationalinstructure',
+    DB: process.env.DB || 'railway',
     PORT: process.env.DB_PORT || 3306,     // use env DB_PORT
     dialect: 'mysql',
 };
