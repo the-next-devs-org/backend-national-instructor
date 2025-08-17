6bc53b58247da3e320300e0712963ddb63cb9f33
The Next Devs <nextdevs6@gmail.com>
2025-08-17 13:53:11 +0500

    Update packageRoutes.js

--- Routes/packageRoutes.js
+++ Routes/packageRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const packageController = require("../Controller/packageController");
-const verifyJWT = require("../middleware/authMiddleware");
+const verifyJWT = require("../Middleware/authMiddleware");
 
 router.post("/createpackage", verifyJWT, packageController.createPackage);
 
